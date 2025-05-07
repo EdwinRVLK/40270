94502ab95a2ce70a94aef98c3009f553a9f012da
RVLK <edumwID_19@hotmail.com>
2025-05-07 16:53:22 -0600

    07-formulario

---
+++ hmtl00/formulario.js
@@ -0,0 +1,9 @@
+console.log("inicio")
+
+function muestra() {
+    let user = document.getElementById("user")
+    let password = document.getElementById("password") 
+
+    console.log(user.value)
+    console.log(password.value)
+}
