608d67d55b7294770281d4804c4ec9c480c233e0
RVLK <edumwID_19@hotmail.com>
2025-05-08 16:52:40 -0600

    09-10 modulos

--- hmtl00/formulario.js
+++ hmtl00/formulario.js
@@ -7,3 +7,26 @@ function muestra() {
     console.log(user.value)
     console.log(password.value)
 }
+
+// ejemplo de función de callback
+const boton = document.getElementById("boton")
+// boton.addEventListener("click", muestra)
+
+// ejemplo de función anónima de tipo arrow
+boton.addEventListener("click", 
+    () => {
+        let user = document.getElementById("user")
+        let password = document.getElementById("password") 
+
+        console.log(user.value)
+        console.log(password.value)
+
+        user.setAttribute("name", "xxx")
+        password.setAttribute("name", "yyy")
+
+        let formulario = document.getElementsByTagName("form")
+        formulario[0].setAttribute("action", "https://www.google.com/search")
+        console.log(formulario[0])
+        formulario[0].submit()
+    }
+)
---
+++ hmtl00/xM.js
@@ -0,0 +1,13 @@
+import { y } from "./yM.js";
+
+function x(params) {
+    y("x")
+    console.log("x: " + params)
+    return ("Fin x")
+}
+
+function xx(params) {
+    return "Fin xx"
+}
+
+export {x, xx}
